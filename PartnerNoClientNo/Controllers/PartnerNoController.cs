4077ddba7e069e75d688a363da3d85bb3e856a0f
LeonidTuz <tuz.leonid7@gmail.com>
2023-05-17 23:18:45 +0300

    Add Response and modified controller

--- PartnerNoClientNo/Controllers/PartnerNoController.cs
+++ PartnerNoClientNo/Controllers/PartnerNoController.cs
@@ -8,33 +8,23 @@ namespace PartnerNoClientNo.Controllers
     [Route("[controller]")]
     public class PartnerNoController : Controller
     {
+
+        Response response = new Response();
+
         [HttpPost]
         public async Task<IActionResult> SetClientNoPartnerNoDueNull()
         {
-            var options = new JsonSerializerOptions
-            {
-                WriteIndented = true
-            };
-
-            var fieldUpdatesList = new List<Field>
-            {
-                new Field { Id = 1, Value = null },
-                new Field { Id = 3, Value = new ChoiceValue { ChoiceIds = new[] { 2 } } },
-                new Field { Id = 5, Value = new ChoiceValue { ChoiceIds = new[] { 2 } } },
-            };
-
-            var response = new FieldUpdates { FieldUpdate =  fieldUpdatesList, ApprovalChoice = "approved"};
+            string json = response.FieldUpdatesJson();
 
             try
             {
-                
-                var json = JsonSerializer.Serialize(response, options);
                 return Ok(json);
             }
             catch (Exception ex)
             {
                 return StatusCode(StatusCodes.Status500InternalServerError);
             }
+
         }
     }
 }
---
+++ PartnerNoClientNo/Models/Pyrus/Response.cs
@@ -0,0 +1,33 @@
+﻿using System.Text.Json;
+
+namespace PartnerNoClientNo.Models.Pyrus
+{
+    public class Response
+    {
+        private readonly JsonSerializerOptions _options;
+
+        public Response() 
+        {
+            _options = new JsonSerializerOptions
+            {
+                WriteIndented = true,
+            };
+        }
+
+        public string FieldUpdatesJson()
+        {
+
+            var fieldUpdatesList = new List<Field>
+        {
+            new Field { Id = 1, Value = null },
+            new Field { Id = 3, Value = new ChoiceValue { ChoiceIds = new[] { 2 } } },
+            new Field { Id = 5, Value = new ChoiceValue { ChoiceIds = new[] { 2 } } },
+        };
+
+            var response = new FieldUpdates { FieldUpdate = fieldUpdatesList, ApprovalChoice = "approved" };
+
+            var json = JsonSerializer.Serialize(response, _options);
+            return json;
+        }
+    }
+}
